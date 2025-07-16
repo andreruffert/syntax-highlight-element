3ccbb3e66b46a2157cd2b3bf8401ec5f85c6f333
André Ruffert <github@andreruffert.com>
2025-07-16 18:57:44 +0200

    feat: introduce `she.config.prismBaseUrl`
    Introduces an optional config option to overwrite the default CDN URL for the Prism package dependencies (tokenizer + language data).

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
 export const NAMESPACE = 'she';
 export const DEFAULT_LANGUAGES = ['markup', 'css', 'javascript'];
+export const DEFAULT_PRISM_BASE_URL = 'https://cdn.jsdelivr.net/npm/prismjs@1.30.0';
 
 /**
  * @typedef Config
  * @type {object}
+ * @property {string} prismBaseUrl - Prism base URL to fetch the tokenizer + language data.
  * @property {string[]} languages - Prism languages.
  * @property {{ [key: string]: string[] }} languageTokens - Language specific token types.
  */
